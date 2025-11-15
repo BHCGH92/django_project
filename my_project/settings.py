ab1bc0ca7ba4ec9be88a7eeaac42b48016b40c27
BHCGH92 <ben.holt1992@live.co.uk>
2025-11-15 19:54:45 +0000

    sorted heroku settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*d#=(ds#lhkvk1vo+p!7*uo&c3tsm5wel&5ew-yg80&2@0xs*v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
